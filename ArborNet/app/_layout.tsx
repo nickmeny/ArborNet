f6c3be684e84c9a05fe30d3a506a3df9b8d9ccaf
nickmeny <nikolasmeny@gmail.com>
2025-11-15 16:01:18 +0200

    delete

--- ArborNet/app/_layout.tsx
+++
@@ -1,8 +0,0 @@
-import { Stack } from "expo-router";
-
-<Stack.Screen options={{ title: "Home" }} />
-export default function RootLayout() {
-  return (
-    <Stack screenOptions={{ headerShown: false }} />
-  );
-}
