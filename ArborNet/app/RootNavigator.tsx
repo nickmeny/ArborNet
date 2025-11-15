13d80ebcf8ae4fd23d0d5f73f575190845492423
maraki-doc <mtgiatra@gmail.com.com>
2025-11-15 13:29:40 +0200

    Marias change: making the form

---
+++ ArborNet/app/RootNavigator.tsx
@@ -0,0 +1,25 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import App from './index'; // <-- Imports your bottom tab bar component
+
+const Stack = createStackNavigator();
+
+const RootNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator 
+        // THIS IS THE FIX for the 'INDEX' header: Hides the header for all screens in the stack
+        screenOptions={{ 
+          headerShown: false 
+        }}
+      >
+        <Stack.Screen
+          name="Index" // This screen name is what creates the "INDEX" title if headerShown is true.
+          component={App}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default RootNavigator;
